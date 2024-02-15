c8f8dd41beed9b495eb8016b05907255971a0e95
Gonzalo Sanchez <gonzalo@ganymede.local>
2024-02-15 01:35:07 -0300

    so we can run bash

--- docker/runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Installs the dependencies with poetry and then runs the application with uvicorn
-# This script is used in the Dockerfile as the entrypoint
-
-poetry install --no-root
-uvicorn src.main:app --reload --host 0.0.0.0 --port 8000
